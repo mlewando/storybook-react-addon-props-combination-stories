8e46a1e33eacd47acbdc0652b3b89a1ead9c23b0
Mateusz Lewandowski <mateusz_lewandowski@epam.com>
2018-01-14 01:08:14 +0100

    Basic story combination addon implementation

--- src/example/TestComponents.story.tsx
+++ src/example/TestComponents.story.tsx
@@ -1,15 +1,28 @@
-import { storiesOf } from "@storybook/react";
+import { storiesOf, Story } from "@storybook/react";
 import * as React from "react";
-import { combinations } from "../ocjectCombinations";
+import { combinations, CombinationValues } from "../ocjectCombinations";
 import { TestComponent, TestComponentProps } from "./TestComponents";
 
-const story = storiesOf("Test component", module).add("Default state", () => (
-  <TestComponent name="sdf" count={3} />
-));
+function combinationStory<P>(
+  values: CombinationValues<P>,
+  name: (props: P) => string,
+  Component: React.StatelessComponent<P> | React.ComponentClass<P>
+): (story: Story) => void {
+  return (story: Story) =>
+    combinations(values).forEach(props =>
+      story.add(name(props), () => <Component {...props} />)
+    );
+}
 
-combinations<TestComponentProps>({
-  count: [2, 3, 4],
-  name: ["Mateusz", "Anka"]
-}).forEach(props =>
-  story.add(`${props.name}: ${props.count}`, () => <TestComponent {...props} />)
+const testStory = storiesOf("Test component", module).add(
+  "Default state",
+  () => <TestComponent name="sdf" count={3} />
 );
+combinationStory(
+  {
+    count: [2, 3, 4],
+    name: ["Mateusz", "Anka"]
+  },
+  props => `${props.name}: ${props.count}`,
+  TestComponent
+)(testStory);
